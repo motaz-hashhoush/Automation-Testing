85b2aaba05bb620a35c45d8801824ea57eb736ec
motaz-hashhoush <motaz.hashhoush@stu.najah.edu>
2022-04-30 03:23:48 +0300

    answer the qustion in profile

--- cypress/integration/skillsmatch/updateSlill.spec.js
+++ cypress/integration/skillsmatch/updateSlill.spec.js
@@ -17,9 +17,38 @@ describe('Update my skills feature', ()=>{
     
 
       //start to test
-      it('sen app', ()=>{
-          
-          cy.xpath('/html/body/div[2]/div[1]/a').click()
+
+      it('answer qustion', ()=>{
+
+          cy.get('a.btn.btn-primary.btn-lg.active.mt-5').click();
+          cy.xpath ('//*[@id="2"]/a').click();
+          cy.xpath('//*[@id="3"]').check()
+          cy.get('[test-data="NextStep"]').click();
+
+          let nextButton = [4, 7, 11, 14, 17, 22, 26];
+          let numQustion = 9;
+          let button = 2;
+          let index = 0;
+
+        
+          for(let qustion = 1; qustion <= 26; qustion++){
+
+            cy.xpath(`//*[@id="${numQustion}"]`).click()
+            numQustion +=5;
+
+            if( qustion == nextButton[index]){
+
+                cy.xpath(`//*[@id="answerForm"]/fieldset[${button}]/input[2]`).click();
+                button++;
+                index++;
+            } 
+          }
+
+       
       })
+
+      it('check the stars')
+
+      
 
 })
